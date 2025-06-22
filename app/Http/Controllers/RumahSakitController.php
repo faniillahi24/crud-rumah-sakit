727ebc51ba8425433078a122d0a757f48df7992c
fani illahi <faniilahi0@gmail.com>
2025-06-23 01:25:07 +0700

    new

--- app/Http/Controllers/RumahSakitController.php
+++ app/Http/Controllers/RumahSakitController.php
@@ -19,19 +19,47 @@ class RumahSakitController extends Controller
         return response()->json($rs);
     }
 
-    public function store(Request $request)
-    {
-        $rs = RumahSakit::create($request->all());
-        return response()->json($rs, 201);
-    }
+   public function store(Request $request)
+{
+    $this->validate($request, [
+        'nama' => 'required|string',
+        'alamat' => 'required|string',
+        'no_telp' => 'required|string',
+        'tipe' => 'required|string',
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
+
+    $rs = RumahSakit::create($request->all());
+    return response()->json([
+        'message' => 'Data berhasil disimpan',
+        'data' => $rs
+    ], 201);
+}
+
 
     public function update(Request $request, $id)
-    {
-        $rs = RumahSakit::find($id);
-        if (!$rs) return response()->json(['message' => 'Data tidak ditemukan'], 404);
-        $rs->update($request->all());
-        return response()->json($rs);
-    }
+{
+    $rs = RumahSakit::find($id);
+    if (!$rs) return response()->json(['message' => 'Data tidak ditemukan'], 404);
+
+    $this->validate($request, [
+        'nama' => 'required|string',
+        'alamat' => 'required|string',
+        'no_telp' => 'required|string',
+        'tipe' => 'required|string',
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
+
+    $rs->update($request->all());
+
+    return response()->json([
+        'message' => 'Data berhasil diperbarui',
+        'data' => $rs
+    ]);
+}
+
 
     public function destroy($id)
     {
